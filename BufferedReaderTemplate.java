2da418cf981e85c1b6454e30843b638885e37c53
Davis Liu <davisliu2006@gmail.com>
2023-09-11 20:55:23 -0400

    Update BufferedReaderTemplate.java

--- BufferedReaderTemplate.java
+++ BufferedReaderTemplate.java
@@ -1,6 +1,40 @@
 import java.io.*;
 import java.util.*;
 
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream in) {
+        br = new BufferedReader(new InputStreamReader(in));
+    }
+    FastScanner(File file) {
+        try {
+            br = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {}
+    }
+
+    String next() {
+        try {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine().trim());
+            return st.nextToken();
+        } catch (IOException e) {return "";}
+    }
+    int nextInt() {return Integer.parseInt(next());}
+    long nextLong() {return Long.parseLong(next());}
+    double nextDouble() {return Double.parseDouble(next());}
+    String nextLine() {
+        try {return br.readLine().trim();}
+        catch (IOException e) {return "";}
+    }
+
+    void close() {
+        try {br.close();}
+        catch (IOException e) {}
+    }
+}
+
 public class BufferedReaderTemplate {
     public static void main(String[] args) throws IOException {
         int n = readInt();
